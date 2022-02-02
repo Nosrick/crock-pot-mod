da00cbbc77fb3084a6ccede1b0a5cdfe8dc7050b
nosrick <nosrick@gmail.com>
2022-02-02 18:30:50 +0000

    Lid now renders with a texture! UVs are still a little off, but we'll get there.

--- src/main/java/com/gitlab/nosrick/crockpot/client/render/block/model/CrockPotBlockEntityRenderer.java
+++ src/main/java/com/gitlab/nosrick/crockpot/client/render/block/model/CrockPotBlockEntityRenderer.java
@@ -1,7 +1,9 @@
 package com.gitlab.nosrick.crockpot.client.render.block.model;
 
 import com.gitlab.nosrick.crockpot.CrockPotMod;
+import com.gitlab.nosrick.crockpot.block.CrockPotBlock;
 import com.gitlab.nosrick.crockpot.blockentity.CrockPotBlockEntity;
+import net.minecraft.block.BlockState;
 import net.minecraft.client.model.*;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumerProvider;
@@ -16,6 +18,7 @@ public class CrockPotBlockEntityRenderer implements BlockEntityRenderer<CrockPot
 
     protected ModelPart lidModel;
     public static EntityModelLayer MODEL_LAYER = new EntityModelLayer(new Identifier(CrockPotMod.MOD_ID, "crock_pot_lid"), "crock_pot_lid");
+    public static Identifier POT_LID_TEXTURE_ID = new Identifier(CrockPotMod.MOD_ID, "textures/block/crock_pot_lid.png");
 
     public CrockPotBlockEntityRenderer(BlockEntityRendererFactory.Context context) {
         this.lidModel = context.getLayerModelPart(MODEL_LAYER);
@@ -25,12 +28,18 @@ public class CrockPotBlockEntityRenderer implements BlockEntityRenderer<CrockPot
     public void render(CrockPotBlockEntity entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
         World world = entity.getWorld();
 
-        if(world == null) {
+        if (world == null) {
+            return;
+        }
+
+        BlockState state = entity.getCachedState();
+
+        if (!state.get(CrockPotBlock.HAS_FOOD)) {
             return;
         }
 
         matrices.push();
-            lidModel.render(matrices, vertexConsumers.getBuffer(RenderLayer.getSolid()), light, overlay);
+        lidModel.render(matrices, vertexConsumers.getBuffer(RenderLayer.getEntitySolid(POT_LID_TEXTURE_ID)), light, overlay);
 
             /*
             MinecraftClient
@@ -57,10 +66,11 @@ public class CrockPotBlockEntityRenderer implements BlockEntityRenderer<CrockPot
         data.getRoot().addChild("crock_pot_lid",
                 ModelPartBuilder
                         .create()
+                        .uv(0, 0)
                         .cuboid(3, 6f, 3f, 10f, 1f, 10f)
                         .cuboid(6f, 7f, 6f, 4f, 1f, 4f),
                 ModelTransform.NONE
         );
-        return TexturedModelData.of(data, 32, 32);
+        return TexturedModelData.of(data, 16, 16);
     }
 }
