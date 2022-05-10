08c84f6360b605840e2f2b6f47a108cde9c42f38
nosrick <nosrick@gmail.com>
2022-05-10 13:49:19 +0100

    Going back to 1.18.2.

--- src/main/java/com/github/nosrick/crockpot/tag/Tags.java
+++ src/main/java/com/github/nosrick/crockpot/tag/Tags.java
@@ -1,21 +1,23 @@
 package com.github.nosrick.crockpot.tag;
 
 import com.github.nosrick.crockpot.CrockPotMod;
-import net.fabricmc.fabric.api.tag.TagFactory;
 import net.minecraft.block.Block;
-import net.minecraft.tag.Tag;
+import net.minecraft.item.Item;
+import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class Tags {
 
     private Tags(){}
 
-    public static Tag.Identified<Block> HEAT_SOURCES = create(new Identifier("c", "heat_sources"), TagFactory.BLOCK);
-    public static Tag.Identified<Block> CROCK_POT_REQUIRES_SUPPORT = create(
-            new Identifier(CrockPotMod.MOD_ID, "crock_pot_requires_support"),
-            TagFactory.BLOCK);
+    public static TagKey<Block> HEAT_SOURCES = createBlockTag(new Identifier("c", "heat_sources"));
+    public static TagKey<Block> CROCK_POT_REQUIRES_SUPPORT = createBlockTag(
+            new Identifier(CrockPotMod.MOD_ID, "crock_pot_requires_support"));
 
-    private static <E> Tag.Identified<E> create(Identifier id, TagFactory<E> tagFactory) {
-        return tagFactory.create(id);
+    private static TagKey<Block> createBlockTag(Identifier id) {
+        return TagKey.of(Registry.BLOCK_KEY, id);
     }
+
+    private static TagKey<Item> createItemTag(Identifier id) { return TagKey.of(Registry.ITEM_KEY, id); }
 }
